4eeb2d3dd7366701aa04ec96e39f5dd6d4063572
Daniel Burgoa <danielburgoa50@gmail.com>
2023-12-25 16:51:41 -0500

    add new task functionality ready

--- script.js
+++ script.js
@@ -1,23 +1,91 @@
 "use strict";
 
 // FUNCTIONS
+const addItem = () => {
+  // Initial conditions
+  const userInput = taskInput.value;
+
+  // Check if input exist
+  if (!userInput) {
+    window.alert(`No task provided. Please introduce a task and try again.`);
+    taskInput.value = "";
+    return 1;
+  }
+
+  // Check if characters are less than 29
+  if (userInput.length > 29) {
+    window.alert(
+      `Limit of characters exceed, please introduce less than 30 characters and try again.`
+    );
+    taskInput.value = "";
+    return 1;
+  }
+
+  const newTask = document.createElement("li");
+  taskList.appendChild(newTask);
+
+  const newIconCheckContainer = document.createElement("div");
+  newIconCheckContainer.classList.add("icon-container");
+  newTask.appendChild(newIconCheckContainer);
+
+  const newCheckBtn = document.createElement("button");
+  newCheckBtn.classList.add("check");
+  newIconCheckContainer.appendChild(newCheckBtn);
+
+  const newCheckIcon = document.createElement("span");
+  newCheckIcon.classList.add("check");
+  newCheckIcon.classList.add("material-symbols-outlined");
+  newCheckIcon.textContent = " done ";
+  newCheckIcon.classList.add("hidden");
+  newCheckBtn.appendChild(newCheckIcon);
+
+  const newTaskContent = document.createElement("p");
+  newTaskContent.textContent = userInput;
+  newTask.appendChild(newTaskContent);
+
+  const newIconDeleteContainer = document.createElement("div");
+  newIconDeleteContainer.classList.add("icon-container");
+  newTask.appendChild(newIconDeleteContainer);
+
+  const newDeleteBtn = document.createElement("button");
+  newDeleteBtn.classList.add("delete");
+  newIconDeleteContainer.appendChild(newDeleteBtn);
+
+  const newDeleteIcon = document.createElement("span");
+  newDeleteIcon.classList.add("delete");
+  newDeleteIcon.classList.add("material-symbols-outlined");
+  newDeleteIcon.textContent = " close ";
+  newDeleteBtn.appendChild(newDeleteIcon);
+
+  taskInput.value = "";
+};
 
 // SELECTORS
 const taskList = document.querySelector(".task-list");
 const taskInput = document.querySelector(".task-input");
 const taskAddBtn = document.querySelector(".task-add");
+const taskForm = document.querySelector(".task-form");
 
 // GLOBAL VARIABLES
 // Maximum amount of tasks = 5
 let taskCounter = 0;
 
-// EVENT LISTENERS
+// EVENT LISTENERS:
+// Prevent default behavior of the form
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// Create a new Task and add it to the list
+taskAddBtn.addEventListener("click", addItem);
+
+// Mark/Unmarked as checked or delete the current item.
 taskList.addEventListener("click", (e) => {
   const item = e.target;
   if (item.classList[0] == "check") {
-    console.log(`Check item`);
+    checkItem();
   } else if (item.classList[0] == "delete") {
-    console.log("Delete item");
+    deleteItem();
   }
 });
 
